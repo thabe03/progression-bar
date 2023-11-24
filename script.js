a1ba1e86f3235b8cea58c305c1bb185c66dc3132
thabe03 <xi1le7@gmail.com>
2023-11-24 03:30:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+window.onscroll = function() {
+    updateProgressBar();
+};
+
+function updateProgressBar() {
+    const winScroll = document.documentElement.scrollTop || document.body.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
